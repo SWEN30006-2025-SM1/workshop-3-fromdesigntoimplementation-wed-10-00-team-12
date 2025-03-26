aaee493e5a895d63f8e4fe682e1cc46d695b99ba
Yuchen0101 <160819428+Yuchen0101@users.noreply.github.com>
2025-03-26 11:22:52 +1100

    'initial commit'

--- app/src/main/java/com/unimelb/swen30006/workshops/Submission.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Submission.java
@@ -1,12 +1,34 @@
 package com.unimelb.swen30006.workshops;
 
-// Sample private submission class to be replaced by your implementation
-class Submission {
-    // Return two fake files
-    public File[] includedFiles(){
-        File[] files = new File[2];
-        files[0] = new File();
-        files[1] = new File();
+public class Submission {
+    private int attemptNum;
+    private File[] files;
+    private int numFiles;
+
+    public Submission(int attemptNum, File[] files) {
+        this.attemptNum = attemptNum;
+        this.files = files;
+        this.numFiles = files.length;
+    }
+
+    public File[] includedFiles() {
         return files;
+    }
+
+    public int getAttemptNum() {
+        return attemptNum;
+    }
+
+    public void setAttemptNum(int attemptNum) {
+        this.attemptNum = attemptNum;
+    }
+
+    public int getNumFiles() {
+        return numFiles;
+    }
+
+    public void setFiles(File[] files) {
+        this.files = files;
+        this.numFiles = files.length;
     }
 }
