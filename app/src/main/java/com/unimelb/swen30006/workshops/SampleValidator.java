b774ccf5e9c5055aadb6d63a5b8198dcc529cf81
songyongjiang <yonsong@student.unimelb.edu.au>
2025-03-26 11:40:20 +1100

    implemented test

--- app/src/main/java/com/unimelb/swen30006/workshops/SampleValidator.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/SampleValidator.java
@@ -11,6 +11,7 @@ public class SampleValidator implements SubmissionValidator{
     public ValidationError[] validateSubmission(Submission submission) {
         ArrayList<ValidationError> errors = new ArrayList<ValidationError>();
         // Loop through all files and create an error if there are any no pdfs
+        //
         File[] files = submission.includedFiles();
         for(File f : files){
             String type = f.fileType();
