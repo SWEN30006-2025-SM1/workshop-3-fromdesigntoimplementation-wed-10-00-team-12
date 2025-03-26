a422ad44e8318f16c3eec41c27767aff0c7d4b86
qingzhao2 <qingyue.zhao@student.unimelb.edu.au>
2025-03-26 11:26:26 +1100

    "first commit"

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Student.java
@@ -0,0 +1,15 @@
+package com.unimelb.swen30006.workshops;
+
+public class Student extends Person {
+    private String email;
+    private String studentID;
+
+    public void sendEmailMsg(String msg) {
+        //
+    }
+
+    public float totalGrade() {
+        //
+        return 0.0f;
+    }
+}
---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Subject.java
@@ -0,0 +1,15 @@
+package com.unimelb.swen30006.workshops;
+
+public class Subject {
+    private String name;
+    private String subjectCode;
+
+    public void createAssignment() {
+
+    }
+
+    public void deleteAssignment(String assignmentName) {
+
+    }
+}
+
