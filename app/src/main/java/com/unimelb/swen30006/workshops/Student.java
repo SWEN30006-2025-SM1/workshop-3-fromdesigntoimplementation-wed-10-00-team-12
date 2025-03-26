480fc0aadef59ebc87a59507656320a86bdd9981
qingzhao2 <qingyue.zhao@student.unimelb.edu.au>
2025-03-26 11:38:27 +1100

    trial commit

--- app/src/main/java/com/unimelb/swen30006/workshops/Student.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Student.java
@@ -5,11 +5,11 @@ public class Student extends Person {
     private String studentID;
 
     public void sendEmailMsg(String msg) {
-        //
+        //implementation here...
     }
 
     public float totalGrade() {
-        //
+        // implementation here...
         return 0.0f;
     }
 }
